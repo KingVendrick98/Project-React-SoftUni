166d7629576431308bac646605d4b76eaa672979
Ivelin Iliev <52979196+KingVendrick98@users.noreply.github.com>
2020-08-12 23:26:50 +0300

    Fixing some colour
    Fixing some colour

--- Front-End/src/components/NavBar/index.js
+++ Front-End/src/components/NavBar/index.js
@@ -61,42 +61,6 @@ function Navigation() {
                 </Link>
             </li>
         </ul>
-
-
-        // <Navbar bg="primary" variant="dark" sticky="top">
-        //     <Navbar.Brand href="/">BlogIT</Navbar.Brand>
-        //     <Nav className="mr-auto">
-        //         {user.loggedIn ?
-        //             <Fragment>
-        //                 <Nav.Link style={{ color: 'white' }} href="/logout">
-        //                     <i className="fas fa-sign-out-alt"></i> Logout
-        //                 </Nav.Link>
-        //                 <Link style={{ color: 'white' }} to="/profile">
-        //                     <i className="fas fa-user-circle"></i> Profile
-        //                 </Link>
-        //                 <DropdownButton id="dropdown-basic-button" variant="primary" title="Posts">
-        //                     <Dropdown.Item href="/myPosts">My Posts</Dropdown.Item>
-        //                     <Dropdown.Item href="/createPost">Create Post</Dropdown.Item>
-        //                     <Dropdown.Item href="/latest">Latest Posts</Dropdown.Item>
-        //                 </DropdownButton>
-        //             </Fragment> :
-        //             <Fragment>
-        //                 <Nav.Link style={{ color: 'white' }} href="/login">
-        //                     <i className="fas fa-door-open"></i> Login
-        //                 </Nav.Link>
-        //                 <Nav.Link style={{ color: 'white' }} href="/register">
-        //                     <i className="fas fa-user-plus"></i> Register
-        //                 </Nav.Link>
-        //             </Fragment>}
-        //         <Nav.Link style={{ color: 'white' }} href="/about">
-        //             <i className="fas fa-info-circle"></i> About
-        //         </Nav.Link>
-        //     </Nav>
-        //     <Form inline style={{ textAlign: "right" }}>
-        //             <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
-        //             <Button variant="outline-light">Search</Button>
-        //     </Form>
-        // </Navbar>
     );
 }
 
