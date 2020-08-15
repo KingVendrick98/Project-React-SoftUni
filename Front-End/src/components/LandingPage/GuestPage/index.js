f5c4f6c5a1b9e7a6daf6e3ce1c3ec1bdf853f015
Ivelin Iliev <52979196+KingVendrick98@users.noreply.github.com>
2020-08-15 04:00:10 +0300

    Added clock
    Added clock

--- Front-End/src/components/LandingPage/GuestPage/index.js
+++ Front-End/src/components/LandingPage/GuestPage/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles.css';
+import Clock from 'react-live-clock';
+
 
 const GuestLandingPage = (props) => {
 
@@ -7,6 +9,9 @@ const GuestLandingPage = (props) => {
         <div className='landingPage'>
             <h1>Welcome to GameWatchers !</h1>
             <h2>The heaven for Gamers</h2>
+            <p>
+            <Clock format={'HH:mm:ss'} ticking={true} timezone={'Sofia'} />
+            </p>
         </div>
     );
 }
